025a4e0988ed0fe7f81a5a589781f9da0f69e499
Kartik Bhatia <kartikbhatia2006@gmail.com>
2021-01-12 21:34:43 +0530

    Writing the code for my assignment. Note that I have checked the code with test cases so it works!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##These functions are just a faster way of doing solve() as it stores data in the cache.
 
 ## Write a short comment describing this function
+## Creating a matrix to store in the cache and defining its setters and getters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv
+  getInverse <- function() inverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## Solving the cached matrix and storing that result in the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("Getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
 }
